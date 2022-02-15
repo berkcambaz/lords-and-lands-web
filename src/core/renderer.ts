02feb968aab74ce8c2035519661be73e264a041d
Berk Cambaz <berkcambaz12321@gmail.com>
2022-02-15 19:03:40 +0300

    Render a triangle

--- src/core/renderer.ts
+++ src/core/renderer.ts
@@ -3,14 +3,12 @@ import { webgl } from "./webgl";
 import vertexShaderSource from "../shaders/vertex.glsl";
 import fragmentShaderSource from "../shaders/fragment.glsl";
 
-console.log(vertexShaderSource);
-
-
 class Renderer {
   public canvas!: HTMLCanvasElement;
 
   public init() {
     this.canvas = document.createElement("canvas");
+    document.body.appendChild(this.canvas);
     webgl.init(this.canvas);
 
     const gl = webgl.gl;
@@ -36,6 +34,33 @@ class Renderer {
     ]
 
     gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(positions), gl.STATIC_DRAW);
+
+    this.canvas.width = window.innerWidth;
+    this.canvas.height = window.innerHeight;
+    gl.viewport(0, 0, window.innerWidth, window.innerHeight)
+
+    // Clear the canvas
+    gl.clearColor(0, 0, 0, 0);
+    gl.clear(gl.COLOR_BUFFER_BIT);
+
+    gl.useProgram(program);
+
+    gl.enableVertexAttribArray(positionAttributeLocation);
+
+    gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
+
+    var size = 2;          // 2 components per iteration
+    var type = gl.FLOAT;   // the data is 32bit floats
+    var normalize = false; // don't normalize the data
+    var stride = 0;        // 0 = move forward size * sizeof(type) each iteration to get the next position
+    var offset = 0;        // start at the beginning of the buffer
+    gl.vertexAttribPointer(
+      positionAttributeLocation, size, type, normalize, stride, offset)
+
+    var primitiveType = gl.TRIANGLES;
+    var offset = 0;
+    var count = 3;
+    gl.drawArrays(primitiveType, offset, count);
   }
 }
 
